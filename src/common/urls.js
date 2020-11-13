f2e3c2181ba131045b1e57fff884643761671dbb
ShyamKatti <shyamstartup@gmail.com>
2020-11-13 15:03:16 +0530

    Updated urls

--- src/common/urls.js
+++ src/common/urls.js
@@ -1,14 +1,14 @@
 const foodAppConstants = {
     services: {
-        PHONE_VALIDATE: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/phonecheck/<phone_number>",
-        OTP_VALIDATE: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/otpcheck",
-        RAZORPAY_ORDER: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/razorpay/order",
-        INIT_ORDER: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/vista/initorder",
-        ADD_ITEM: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/vista/additemorder",
-        EXTEND_TIME: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/vista/extendtimeout",
-        PLACE_ORDER: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/vista/placeorder",
-        DELIVERY_URL: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/vista/delivery",
-        VALIDATE_BOOKING_ID_URL: "https://cinecafebackend-dev.ap-south-1.elasticbeanstalk.com/vista/validate",
+        PHONE_VALIDATE: "https://api.bmxcinemas.com/phonecheck/<phone_number>",
+        OTP_VALIDATE: "https://api.bmxcinemas.com/otpcheck",
+        RAZORPAY_ORDER: "https://api.bmxcinemas.com/razorpay/order",
+        INIT_ORDER: "https://api.bmxcinemas.com/vista/initorder",
+        ADD_ITEM: "https://api.bmxcinemas.com/vista/additemorder",
+        EXTEND_TIME: "https://api.bmxcinemas.com/vista/extendtimeout",
+        PLACE_ORDER: "https://api.bmxcinemas.com/vista/placeorder",
+        DELIVERY_URL: "https://api.bmxcinemas.com/vista/delivery",
+        VALIDATE_BOOKING_ID_URL: "https://api.bmxcinemas.com/vista/validate",
         MENU_URL: "https://s3.ap-south-1.amazonaws.com/balaji.cinecafe/assets/menu.csv"
     }
 };
